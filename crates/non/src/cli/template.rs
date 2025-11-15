4db40d510b70c7d461d713e95c3847d243d1cb7e
kjuulh <contact@kjuulh.io>
2025-11-15 15:17:55 +0100

    feat: add render template (missing injection of config
    Signed-off-by: kjuulh <contact@kjuulh.io>

--- crates/non/src/cli/template.rs
+++ crates/non/src/cli/template.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use anyhow::Context;
+use tokio::io::AsyncWriteExt;
 
 use crate::{
     non_context::NonContextState,
@@ -55,10 +56,6 @@ impl TemplateCommand {
                 continue;
             };
 
-            if let Some("jinja2") = template_file.extension().and_then(|f| f.to_str()) {
-                todo!("templating not implemented yet");
-            }
-
             let new_file_name = file_name.to_str().unwrap().trim_end_matches(".jinja2");
             let mut new_file_path = temp_dir.join(file);
             new_file_path.set_file_name(new_file_name);
@@ -72,18 +69,89 @@ impl TemplateCommand {
                     ))?;
             }
 
-            tracing::info!(from = %template_file.display(), to = %new_file_path.display(), "templating file");
-            tokio::fs::copy(&template_file, &new_file_path)
-                .await
-                .context(anyhow::anyhow!(
-                    "copying file: from {} - to {}",
-                    template_file.display(),
-                    new_file_path.display()
-                ))?;
+            if let Some("jinja2") = template_file.extension().and_then(|f| f.to_str()) {
+                tracing::info!(from = %template_file.display(), to = %new_file_path.display(), "templating file");
+
+                let raw_content = tokio::fs::read_to_string(template_file)
+                    .await
+                    .context("read template file")?;
+                let rendered_content = render_template(&raw_content).context("render template")?;
+
+                let mut file = tokio::fs::File::create(&new_file_path)
+                    .await
+                    .context("create template file")?;
+                file.write_all(rendered_content.as_bytes())
+                    .await
+                    .context("write template content")?;
+                file.flush().await.context("flush file")?;
+            } else {
+                tracing::info!(from = %template_file.display(), to = %new_file_path.display(), "copying file");
+
+                tokio::fs::copy(&template_file, &new_file_path)
+                    .await
+                    .context(anyhow::anyhow!(
+                        "copying file: from {} - to {}",
+                        template_file.display(),
+                        new_file_path.display()
+                    ))?;
+            }
         }
 
         // Output to tmp destination
 
         Ok(())
+    }
+}
+
+fn render_template(template_content: &str) -> anyhow::Result<String> {
+    let mut env = minijinja::Environment::new();
+    env.set_debug(true);
+    env.add_filter("to_lower", |input: String| -> String {
+        input.to_lowercase()
+    });
+    env.add_filter("to_upper", |input: String| -> String {
+        input.to_uppercase()
+    });
+    env.add_filter("to_snake", |input: String| -> String {
+        stringcase::snake_case(&input)
+    });
+    env.add_filter("to_camel", |input: String| -> String {
+        stringcase::camel_case(&input)
+    });
+    env.add_filter("to_pascal", |input: String| -> String {
+        stringcase::pascal_case(&input)
+    });
+    env.add_filter("to_screaming_snake", |input: String| -> String {
+        stringcase::macro_case(&input)
+    });
+    env.add_filter("to_kebab", |input: String| -> String {
+        stringcase::kebab_case(&input)
+    });
+    env.add_filter(
+        "as_bool",
+        |input: String| -> Result<bool, minijinja::Error> {
+            input.parse::<bool>().map_err(|e| {
+                minijinja::Error::new(minijinja::ErrorKind::InvalidOperation, e.to_string())
+            })
+        },
+    );
+
+    let res = env.render_str(template_content, minijinja::context! {});
+
+    match res {
+        Ok(output) => Ok(output),
+        Err(e) => {
+            let mut error_causes = Vec::new();
+
+            error_causes.push(format!("template error: {e:#}\n\n"));
+
+            let mut err = &e as &dyn std::error::Error;
+            while let Some(next_err) = err.source() {
+                error_causes.push(format!("caused by: {:#}", next_err));
+                err = next_err;
+            }
+
+            anyhow::bail!("{}", error_causes.join("\n\n"));
+        }
     }
 }
